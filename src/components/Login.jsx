aa010b77da865b5e6f5873c5d2af6ff057f1d559
LEOM <leom3108@outlook.com>
2026-06-23 17:24:54 -0300

    atualizacao-css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,11 @@
 function Login(){
     return(
         <>
+            <div className="container">
+                <input type="text" placeholder="usuário"/>
+                <input type="password" placeholder="senha"/>
+                <button>Enviar</button>
+            </div>
             <h2>Login</h2>
         </>
         
