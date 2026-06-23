3758d7dd55606732e70d6e2377dbf94db9240c1b
LEOM <leom3108@outlook.com>
2026-06-23 16:12:42 -0300

    unico-header

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Login from "./components/Login";
 function App(){
     return(
         <>
-            <Header />
+            <Header titulo="Lanchonete Xpress" 
+            subtitulo="O melhor da região" />
             <Login />
         </>
     )
