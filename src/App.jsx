6ccd1571ba3c07a3b60e9f2244f9bbee0a38c626
leom <leom3108@outlook.com>
2026-07-01 17:19:33 -0300

    card array

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ import Contador from "./components/Contador";
 import Calculadora from "./components/Calculadora";
 
 function App(){
+    const lanches = [{id:1, nome:"X-Burguer", preco:15.90},
+                     {id:2, nome:"X-Salada", preco:17.90},
+                     {id:3, nome:"Refrigerante", preco:6.00}]
     return(
         <>
             <Header titulo="Lanchonete Xpress" 
             subtitulo="O melhor da região" />
             <Login />
 
-            <CardProd nome="X-Burguer" preco={15.90} />
-            <CardProd nome="X-Salada" preco={17.90} />
-            <CardProd nome="Refrigerante" preco={6.00} />
-
+            {lanches.map(lanche=> (
+                <CardProd key={lanche.id} nome={lanche.nome} preco={lanche.preco} />
+            ))}
+  
             <Calculadora />
 
             <Funcionario func="João" cargo="Atendente" />
